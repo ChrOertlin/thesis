87f5e2faa45567553d03ac1f97f6b0302d5c08ed
Christian Oertlin <christian.oertlin@ki.se>
2021-02-14 22:41:09 +0100

    introDogma

--- build_thesis.R
+++ build_thesis.R
@@ -1,6 +1,5 @@
-setwd("/home/chroer/projects/thesis/phdthesis_ki-master/")
-setwd(
+#setwd("/home/chroer/projects/thesis/phdthesis_ki-master/")
+setwd("C:/Users/coert/work/thesis/phdthesis_ki-master/")
 library(bookdown)
 bookdown::render_book("index.Rmd")
 
-bookdown::render_book("")
